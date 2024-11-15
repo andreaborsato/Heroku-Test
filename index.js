2cfb36c36e7fd57092a458e37e993cb5036731d4
4ndre416 <andre.borsato16@gmail.com>
2024-11-15 23:51:53 +0100

    image saving update

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const http = require("http");
 const socketIO = require("socket.io");
 const cors = require("cors");
 const { join } = require("node:path");
+const fs = require("fs");
 
+// Array storing clients ids
 let myClients = [];
-
-// Name Clients to send them data
 
 // Create an Express app
 const app = express();
@@ -26,18 +26,12 @@ const io = socketIO(server, {
   },
 });
 
-// // Serve static files from the public directory
-// app.use(express.static("public"));
-
 // Serve static files from the 'public' folder
 app.use(express.static(join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
-
-// Assuming 'clients' is an array that stores objects with 'customId' and 'clientId'
-var clients = {};
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
@@ -48,9 +42,20 @@ io.on("connection", (socket) => {
 
   io.to(id).emit("id_assignment", id);
 
+  socket.on("image_save", (data) => {
+    console.log(data);
+
+    var img = data;
+    // strip off the data: url prefix to get just the base64-encoded bytes
+    var myPng = img.replace(/^data:image\/\w+;base64,/, "");
+    var buf = Buffer.from(myPng, "base64");
+    fs.writeFile("./public/images/image.png", buf, function (err) {
+      if (err) throw err;
+    });
+  });
+
   socket.on("disconnect", function () {
     // Remove the disconnected client from the clients object
-
     delete myClients[id];
     removeItem(myClients, id);
     console.log("Updated client list: " + myClients);
@@ -63,6 +68,7 @@ server.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port http://0.0.0.0:${port}`);
 });
 
+// Update array of clients ids
 function removeItem(array, itemToRemove) {
   const index = array.indexOf(itemToRemove);
 
--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,31 @@
+let bgColor = {
+  r: Number,
+  g: Number,
+  b: Number,
+};
+
+let myCv;
+
 function setup() {
-  createCanvas(400, 400);
+  myCv = createCanvas(800, 800);
+  bgColor.r = Math.round(random(255));
+  bgColor.g = Math.round(random(255));
+  bgColor.b = Math.round(random(255));
 }
 
 function draw() {
-  background(220);
+  background(bgColor.r, bgColor.g, bgColor.b);
+
+  textSize(30);
+  text(`Background Color: ${bgColor.r} ${bgColor.g} ${bgColor.b}`, 10, 50);
+}
+
+function keyPressed() {
+  if (key === "d") {
+    console.log("Canvas Saved!");
+    let image64 = myCv.canvas.toDataURL();
+    console.log(image64);
+
+    socket.emit("image_save", image64);
+  }
 }
